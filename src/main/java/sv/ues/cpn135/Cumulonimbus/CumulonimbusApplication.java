117f1ff5e7ace67f374052c5913928ca25d04d96
DJeovannyZ <darwinzaldana@gmail.com>
2025-10-30 23:13:12 -0600

    Inicio del proyecto

---
+++ src/main/java/sv/ues/cpn135/Cumulonimbus/CumulonimbusApplication.java
@@ -0,0 +1,13 @@
+package sv.ues.cpn135.Cumulonimbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumulonimbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumulonimbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/ues/cpn135/Cumulonimbus/CumulonimbusApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.ues.cpn135.Cumulonimbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumulonimbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
