7257d21b15779415de4465af10faf0c282f52234
DJeovannyZ <darwinzaldana@gmail.com>
2025-11-07 23:55:42 -0600

    ignoring s3 service

--- src/main/java/sv/ues/cpn135/Cumulonimbus/service/impl/S3ServiceImpl.java
+++ src/main/java/sv/ues/cpn135/Cumulonimbus/service/impl/S3ServiceImpl.java
@@ -11,24 +11,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class S3ServiceImpl implements StorageCloudService {
-
-  private final S3Client s3Client;
-
-  @Value("${AWS_BUCKET_NAME}")
-  private String bucketName;
-
-  public S3ServiceImpl(S3Client s3Client) {
-    this.s3Client = s3Client;
-  }
-
-  public void uploadFile(String keyName, String filePath) {
-    PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-        .bucket(bucketName)
-        .key(keyName)
-        .build();
-
-    PutObjectResponse response = s3Client.putObject(putObjectRequest, Paths.get(filePath));
-    System.out.println("Archivo subido a s3: " + response.eTag());
-  }
+public class S3ServiceImpl /* implements StorageCloudService */ {
+  /*
+   * private final S3Client s3Client;
+   * 
+   * @Value("${AWS_BUCKET_NAME}")
+   * private String bucketName;
+   * 
+   * public S3ServiceImpl(S3Client s3Client) {
+   * this.s3Client = s3Client;
+   * }
+   * 
+   * public void uploadFile(String keyName, String filePath) {
+   * PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+   * .bucket(bucketName)
+   * .key(keyName)
+   * .build();
+   * 
+   * PutObjectResponse response = s3Client.putObject(putObjectRequest,
+   * Paths.get(filePath));
+   * System.out.println("Archivo subido a s3: " + response.eTag());
+   * }
+   */
 }
